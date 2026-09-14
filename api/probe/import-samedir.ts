3f7d266e43ea24a72cb2d907017032ce194449bc
Soham Das <dassoham345@gmail.com>
2026-09-14 15:41:06 +0530

    Remove stale samedir probe

--- api/probe/import-samedir.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { PROBE_CONST } from "./_constant";
-
-export default function handler(_req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ ok: true, probe: "import-samedir", value: PROBE_CONST });
-}
